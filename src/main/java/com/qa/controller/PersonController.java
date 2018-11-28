b8248fd88cb2ce1efaf329a37410042dfebbee65
scrappy1987 <jagordon87@msn.com>
2018-11-28 11:04:45 +0000

    second commit

--- src/main/java/com/qa/controller/PersonController.java
+++ src/main/java/com/qa/controller/PersonController.java
@@ -2,10 +2,8 @@ package com.qa.controller;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
